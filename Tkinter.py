348014f38a4ec65e76845601f7f6b4a8b74694bb
demi <hang10z@gmail.com>
2015-09-30 18:18:41 -0400

    1

--- Tkinter.py
+++ Tkinter.py
@@ -21,12 +21,37 @@ def p1_move_N(self):
     canvas.create_line(p1_x, p1_y, p1_x, (p1_y-line_length), width=line_width, fill=p1_colour)
     p1_y = p1_y - line_length
 
+def p1_move_S(self):
+    global p1_y
+    canvas.create_line(p1_x, p1_y, p1_x, p1_y+line_length, width=line_width, fill=p1_colour)
+    p1_y = p1_y + line_length
+
+def p1_move_E(self):
+    global p1_x
+    canvas.create_line(p1_x, p1_y, p1_x + line_length, p1_y, width=line_width, fill=p1_colour)
+    p1_x = p1_x + line_length
+
+def p1_move_W(self):
+    global p1_x
+    canvas.create_line(p1_x, p1_y, p1_x - line_length, p1_y, width=line_width, fill=p1_colour)
+    p1_x = p1_x - line_length
+
+def erase_all(self):
+    canvas.delete(all)
 
 ##### main:
 window = Tk()
 window.title("MyEtchASketch")
 canvas = Canvas(bg=canvas_colour, height=canvas_height, width=canvas_width, highlightthickness=0)
 canvas.pack()
+
+# bind movement to key presses
+
+window.bind("<Up>", p1_move_N)
+window.bind("<Down>", p1_move_S)
+window.bind("<Left>", p1_move_W)
+window.bind("<Right>", p1_move_E)
+window.bind("u", erase_all)
 
 window.mainloop()
 
